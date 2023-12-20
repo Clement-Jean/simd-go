b6693c9f4f1014702b9c004d3f3d44c4ea64fc9f
Clement <clement.jean@epitech.eu>
2023-12-20 12:21:02 +0800

    doc: add type descriptions

--- type.go
+++ type.go
@@ -1,9 +1,19 @@
 package simdgo
 
+// A SIMD vector with 16 elements of type uint8
 type Uint8x16 [16]uint8
+
+// A SIMD vector with 32 elements of type uint8
 type Uint8x32 [32]uint8
+
+// A SIMD vector with 64 elements of type uint8
 type Uint8x64 [64]uint8
 
+// A SIMD vector with 16 elements of type int8
 type Int8x16 [16]int8
+
+// A SIMD vector with 32 elements of type int8
 type Int8x32 [32]int8
+
+// A SIMD vector with 64 elements of type int8
 type Int8x64 [64]int8
